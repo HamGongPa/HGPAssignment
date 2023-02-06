649e0d4872409226ccf3dfa80f2ef2be33b43655
jaeeun-choi36 <124008261+jaeeun-choi36@users.noreply.github.com>
2023-02-06 17:09:16 +0900

    Delete 사전과제.py

--- 사전과제.py
+++
@@ -1,4 +0,0 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("최재은")
-최재은
