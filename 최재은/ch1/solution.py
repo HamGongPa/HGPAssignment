d5c95faac1661eb706cdd61afde21047c29bee4a
unknown <lydia36j@gmail.com>
2023-02-20 16:16:26 +0900

    1주차 과제제출

---
+++ 최재은/ch1/solution.py
@@ -0,0 +1,31 @@
+# 두 수의 합
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+# 두 수의 차
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+# 두 수의 곱
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
+
+# 두 수의 나눗셈
+def solution(num1, num2):
+    answer = int((num1 / num2) * 1000)
+    return answer
+
+# 몫 구하기
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+# 나머지 구하기
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+
