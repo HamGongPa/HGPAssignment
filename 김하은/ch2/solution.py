9ca6dda25b093b01a635928b28823463b7ed6d11
Heidi Kim <90129037+heidi127kim@users.noreply.github.com>
2023-02-28 09:48:49 +0900

    Update solution.py

--- 김하은/ch2/solution.py
+++ 김하은/ch2/solution.py
@@ -1,19 +1,19 @@
-양꼬치 (2주차 문제풀이)
+#양꼬치 (2주차 문제풀이)
 def solution1(n, k):
     answer = n*12000 + k*2000 - (n//10)*2000
     return answer
 
-특정 문자 제거하기
+#특정 문자 제거하기
 def solution2(my_string, letter):
     answer = my_string.replace(letter,"")
     return answer
 
-문자열 뒤집기
+#문자열 뒤집기
 def solution3(my_string):
     answer = my_string[::-1]
     return answer
 
-문자열 대문자로 변환
+#문자열 대문자로 변환
 def solution4():
     my_string = input("문자열을 입력하세요>")
     print("원본:",my_string)
