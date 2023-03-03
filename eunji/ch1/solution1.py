c1011e89382fa4bf590931cf7bebd3a8f12a0ee1
unknown <fufume2414@gmail.com>
2023-03-03 22:19:21 +0900

    1주차 과재 제출

---
+++ eunji/ch1/solution1.py
@@ -0,0 +1,29 @@
+#두 수의 합
+def solution1(num1, num2):
+    answer = num1+num2
+    return answer
+
+#두 수의 차
+def solution2(num1, num2):
+    answer = num1-num2
+    return answer
+
+#두 수의 곱
+def solution3(num1, num2):
+    answer = num1*num2
+    return answer
+
+#두 수의 나누기
+def solution4(num1, num2):
+    answer = num1/num2*1000
+    return int(answer)
+
+#몫 구하기
+def solution5(num1, num2):
+    answer = num1//num2
+    return answer
+
+#나머지 구하기
+def solution6(num1, num2):
+    answer = num1%num2
+    return answer
